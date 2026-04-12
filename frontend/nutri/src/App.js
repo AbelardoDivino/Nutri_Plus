29fb0c3b56814921eafab2c8f8374beea5f4ef84
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-04-12 18:18:20 -0300

    update

--- frontend/nutri/src/App.js
+++ frontend/nutri/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navbar>  </Navbar>
       </header>
       <Login></Login>
     </div>
---
+++ frontend/nutri/src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+        <div>
+            <a>sobre</a>
+            <a>mais</a>
+        </div>
+    )
+}
+
+export default Navbar
