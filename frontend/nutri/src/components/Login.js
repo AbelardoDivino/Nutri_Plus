0b1afd29161938a4d32c8fb8d287f0e4fc5b064e
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-04-14 15:02:46 -0300

    update css and node

--- frontend/nutri/src/components/Login.js
+++ frontend/nutri/src/components/Login.js
@@ -1,12 +1,21 @@
 import { useState } from "react"
 function Login(){
     return(
-        <div>
+        <div className="divprincinpaldologin">
             <label>Nome:
                 <div>
                     <input type="text" placeholder="Digite seu nome" minLength={8} maxLength={40}></input>
                 </div>
+                <label> Senha
+                <div>
+                    <input type="password"placeholder="min 8 caracters"></input>
+                </div>
+                </label>
             </label>
+            <div id="botoesparaentrar">
+            <button id="entrar">Entrar</button> 
+            <button id="admin"> Admin</button>       
+         </div>
         </div>
         
     )
