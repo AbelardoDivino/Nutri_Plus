b80d3c94ee47f7a7845ab2575e20377afe5fb4b8
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-04-15 22:26:06 -0300

    update css

--- frontend/nutri/src/components/Navbar.js
+++ frontend/nutri/src/components/Navbar.js
@@ -1,6 +1,7 @@
 function Navbar(){
     return(
         <div>
+            <img src='/imagens/imagenlogo.png' alt="logo" id="fotologo"></img>
             <a>sobre</a>
             <a>mais</a>
         </div>
